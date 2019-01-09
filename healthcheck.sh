9310c3772d89c5c08c4af7064055972ae6a12cfd
Ryan Schlesinger <ryan@ryanschlesinger.com>
2019-01-09 12:53:25 -0800

    Update compose example

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/srcon --ip=127.0.0.1 --port=27500 --password=$PASSWORD status | grep "GameStatus : Running"
+/usr/local/bin/srcon --ip=127.0.0.1 --port=27500 --password=$HEALTHCHECK_PASSWORD status | grep "GameStatus : Running"
