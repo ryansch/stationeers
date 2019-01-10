88f6c7aeeda15eae21c1ec424a1e26ff3bf475bf
Ryan Schlesinger <ryan@ryanschlesinger.com>
2019-01-10 12:09:18 -0800

    Use rcon password from default.ini

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,16 @@ if [ ! -f $BASE/default.ini ]; then
   sed -i s/RCONPASSWORD=stationeers/RCONPASSWORD=$(pwgen 15 1)/g $BASE/default.ini
 fi
 
+RCONPASSWORD=$(egrep 'RCONPASSWORD=.+$' $BASE/default.ini | cut -d '=' -f2 | tr -d '\n\r')
+
+cat << EOF > /root/.srcon
+{
+  "password": "$RCONPASSWORD",
+  "hostname": "127.0.0.1",
+  "port": "27500"
+}
+EOF
+
 exec /opt/stationeers/rocketstation_DedicatedServer.x86_64 \
   -batchmode -nographics -autostart \
   -basedirectory=$BASE \
